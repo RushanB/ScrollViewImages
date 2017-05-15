9af3bdb7f3a15f6b9c34623b87eba1d4d10a074a
RsuhanB <Rushanben@hotmail.com>
2017-05-15 14:17:47 -0400

    First Commit

---
+++ ScrollViewImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImages
+//
+//  Created by Rushan on 2017-05-15.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImages/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ScrollViewImages
+//
+//  Created by Rushan on 2017-05-15.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
+@end
+
+@implementation ViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIImageView *
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
